6d785188d9f40e2069fa60d6547ecd43fe6d0ed6
ukwuani <ukwuanibarnabas@gmail.com>
2026-01-17 23:52:33 -0800

    Video moved to cloud

--- tacc-webpages/src/HeaderVideo.tsx
+++ tacc-webpages/src/HeaderVideo.tsx
@@ -11,7 +11,7 @@ export default function VideoPlayer() {
       style={{ width: "100%", borderRadius: '16px', boxShadow: '0 20px 60px rgba(0, 0, 0, 0.1)', pointerEvents: 'none'}}
       
     >
-      <source src="/vid/tacc.mp4" type="video/mp4" />
+      <source src="https://res.cloudinary.com/echwood/video/upload/v1768722643/tacc_eukisc.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
