38d91a7cc76d986b31fb6675771801da0ba51363
ukwuani <ukwuanibarnabas@gmail.com>
2026-01-24 14:55:33 -0800

    add video description

--- tacc-webpages/src/HeaderVideo.tsx
+++ tacc-webpages/src/HeaderVideo.tsx
@@ -9,8 +9,10 @@ export default function VideoPlayer() {
       playsInline
       preload="auto"
       style={{ width: "100%", borderRadius: '16px', boxShadow: '0 20px 60px rgba(0, 0, 0, 0.1)', pointerEvents: 'none'}}
+      aria-label={"The Automation Control Center | TACC Video"}
       
     >
+      
       <source src="https://res.cloudinary.com/echwood/video/upload/v1768722643/tacc_eukisc.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
